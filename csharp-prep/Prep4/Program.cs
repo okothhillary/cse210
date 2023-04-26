37399667fa2096edc9ac1272f02239c163a267a0
Okothhillary <hobboltport@gmail.com>
2023-04-26 18:43:33 +0300

    Weird Calculator

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,33 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
+
+    {  List<int> numbers = new List<int>();
+       int number = -1;
+
+       while (number != 0)       
+       {
+            Console.Write("Give me a number: ");
+            number = int.Parse(Console.ReadLine());
+
+            numbers.Add(number);
+       }
+       
+        int sum = numbers.Sum();
+
+        int max = numbers.Max();
+
+        double average = numbers.Average();
+
+        Console.WriteLine($"The sum is {sum}");  
+
+        Console.WriteLine($"The maximum number is {max}");  
+
+        Console.WriteLine($"The average is {average}");
+
+          
+
     }
 }
